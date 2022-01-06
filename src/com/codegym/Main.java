dbd800a800f1aeb987ef0d155b2fa866f1a7ebb4
ruukichi <ngovietlc93@gmail.com>
2022-01-06 15:46:36 +0700

    them phan tu vao mang

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,33 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] array = {"Viet", "Binh", "Luyt", "Toan"};
+        System.out.println("---Mảng ban đầu---");
+        for (String element : array) {
+            System.out.println(element);
+        }
+        String[] newArray = addNewElementToArray(array, 1, "Sang");
+        System.out.println("---Mảng sau khi thêm---");
+        for (int i = 0; i < newArray.length; i++) {
+            System.out.println(newArray[i]);
+        }
+
+    }
+
+    public static String[] addNewElementToArray(String[] array, int index, String value) {
+        String[] newArray = new String[array.length + 1];
+        for (int i = 0; i < newArray.length; i++) {
+            if (i < index) {
+                newArray[i] = array[i];
+            } else if (i == index) {
+                newArray[i] = value;
+            } else {
+                newArray[i] = array[i - 1];
+            }
+        }
+        return newArray;
+    }
+
+}
